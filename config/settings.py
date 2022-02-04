165adf792893693be9b543a98dda9e14720e76d4
Nodir-tech <77410370+Nodir-tech@users.noreply.github.com>
2022-02-04 20:49:23 +0500

    Heroku uchun o`zgartirishlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2e$fez$gdauzc!rov(s%e7q_qu5d9=r0li25eo%)_s3a*qk_%-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
